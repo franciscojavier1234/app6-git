759eeef2ce739dbe8c41ac76ea8438ef5aa21fcd
franciscojavier1234 <gerardo.hernandez.6923@gmail.com>
2019-05-08 10:31:07 -0500

    3subir

--- js/acciones.js
+++ js/acciones.js
@@ -4,11 +4,11 @@ $(document).ready(function(e){
 	document.addEventListener("deviceready",function(){
 		
 		$('#izquierda').on("swipeleft",function(){
-			navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicacion ","Aceptar"});
+			navigator .notification. alert("Deslizo a la izquierda",function(){"Aplicacion ","Aceptar"});
 		});
 		
 		$('#derecha').on("swiperight",function(){
-			navigator.notification.confirm ("¿Que quieres hacer?",function(opt){
+			navigator .notification. confirm ("¿Que quieres hacer?",function(opt){
 				switch(opt)
 				{
 					case 1:
